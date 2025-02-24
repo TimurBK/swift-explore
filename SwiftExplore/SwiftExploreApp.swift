32f990b3f84cc4dc5c17f8ac413a5dc505e3b5a5
Timur Kuchkarov <timurbk91@gmail.com>
2025-02-24 01:18:19 +0000

    Create Xcode project

---
+++ SwiftExplore/SwiftExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExploreApp.swift
+//  SwiftExplore
+//
+//  Created by Timur Kuchkarov on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftExploreTests/SwiftExploreTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExploreTests.swift
+//  SwiftExploreTests
+//
+//  Created by Timur Kuchkarov on 24/02/2025.
+//
+
+import Testing
+@testable import SwiftExplore
+
+struct SwiftExploreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
